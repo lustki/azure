36ab648f52a598b45c82df88af4379c7b17fd03a
lucka_a <anna.lucka2002@gmail.com>
2025-12-02 21:13:09 +0100

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>Witaj w dynamicznej aplikacji Flask!</h1><p>Aktualny czas serwera: {current_time}</p>"
+    return f"<h1>Witaj! Aplikacja wdrożona przez GitHub Actions!</h1><p>Aktualny czas serwera: {current_time}</p>"
 
 if __name__ == '__main__':
     app.run()
